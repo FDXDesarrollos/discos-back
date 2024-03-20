54cee043abe992dc3201b36fa99370a9810bb8b1
fjuarez <fdxdesarrollos@gmail.com>
2024-03-19 18:09:23 -0600

    se corrige bug JwtProvider - refreshToken

--- src/main/java/net/fdxdesarrollos/albums/security/jwt/JwtProvider.java
+++ src/main/java/net/fdxdesarrollos/albums/security/jwt/JwtProvider.java
@@ -83,17 +83,23 @@ public class JwtProvider {
 	}
 	
 	public String refreshToken(JwtDto jwtDto) throws ParseException {
-		JWT jwt = JWTParser.parse(jwtDto.getToken());
-		JWTClaimsSet claims = jwt.getJWTClaimsSet();
-		String usuario = claims.getSubject();
-		List<String> roles = (List<String>) claims.getClaim("roles");
+		try {
+			Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(jwtDto.getToken());
+		} catch (ExpiredJwtException ex) {
+			JWT jwt = JWTParser.parse(jwtDto.getToken());
+			JWTClaimsSet claims = jwt.getJWTClaimsSet();
+			String usuario = claims.getSubject();
+			List<String> roles = (List<String>) claims.getClaim("roles");
+			
+			return Jwts.builder()
+					   .setSubject(usuario)
+					   .claim("roles", roles)
+					   .setIssuedAt(new Date())
+					   .setExpiration(new Date(new Date().getTime() + expiration))
+					   .signWith(SignatureAlgorithm.HS512, secret.getBytes())
+					   .compact();			
+		}
 		
-		return Jwts.builder()
-				   .setSubject(usuario)
-				   .claim("roles", roles)
-				   .setIssuedAt(new Date())
-				   .setExpiration(new Date(new Date().getTime() + expiration))
-				   .signWith(SignatureAlgorithm.HS512, secret.getBytes())
-				   .compact();		
+		return null;
 	}
 }
